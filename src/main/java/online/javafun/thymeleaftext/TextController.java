d1253372b93ad0c920c13de72b2c96c9b57f187d
User <user@its.me>
2023-01-26 07:14:53 +0100

    thymeleaf text protection

--- src/main/java/online/javafun/thymeleaftext/TextController.java
+++ src/main/java/online/javafun/thymeleaftext/TextController.java
@@ -1,2 +1,15 @@
-package online.javafun.thymeleaftext;public class TextController {
+package online.javafun.thymeleaftext;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TextController {
+
+    @GetMapping("/")
+    String home(Model model) {
+        model.addAttribute("dangerousCode", "<script>alert('Masz wirusa');</script>");
+        return "index";
+    }
 }
--- src/test/java/online/javafun/thymeleaftext/ThymeleafTextApplicationTests.java
+++ src/test/java/online/javafun/thymeleaftext/ThymeleafTextApplicationTests.java
@@ -8,3 +8,6 @@ class ThymeleafTextApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+}
